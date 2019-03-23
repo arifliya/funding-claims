f2fb34ef030d40b950d2cad4796344c2212f8bbe
Arif Liya <arifliya@gmail.com>
2019-03-23 01:12:40 +0000

    new design and functionality on adding

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,48 +24,210 @@ $('#confirmOrView').click(function() {
 
 
 // $(".inner").blur(function() {
+//   var input = $(this).val();
+//   if($(this).val() == ""){
+//     return false;
+//   }
+//   if($(this).val().indexOf('£' ) > -1){
+//     return false;
+//   }
+//   if($(this).val() != "£"){
+//     $( this ).val( '£' + input );
+//   }
+// });
+//
+//
+// $(".inner").blur(function() {
+//   var input = $(this).val();
+//   if($(this).val() == ""){
+//      return false;
+//    }
+//   if($(this).val().indexOf('.' ) > -1){
+//      return false;
+//    }
+//   if($(this).val() != "."){
+//      $( this ).val( input + '.00' );
+//    }
+// });
+
+// $(function() {
+//   $('.fig-edit1').on("input", function() {
+//     $('.1').text($(this).val());
+//   });
+// });
+//
+// $(function() {
+//   $('.fig-edit2').on("input", function() {
+//     $('.2').text($(this).val());
+//   });
+// });
+//
+// var a = parseInt($(".1").text());
+// var b = parseInt($(".2").text());
+//
+// $(".fig-edit1").blur(function() {
+//   $('#total').text('');
+//   $('#total').append(a+b);
+// });
+//
+// $(".fig-edit2").blur(function() {
+//   $('#total').text('');
+//   $('#total').append(a+b);
+// });
+
+// var total = 0;
+// $('.fig').each(function(){
+//   total += parseInt($(this).text());
+// });
+// $('#total').append("£"+total);
+//
+// $(".fig-edit").blur(function() {
 //
 //   var input = $(this).val();
 //
 //   if($(this).val() == ""){
-//      return false;
-//    }
+//       return false;
+//     }
+// });
 //
-//   if ($(this).val().indexOf('.' ) > -1) {
-//     $( this ).val( input );
-//   }
+// var a = parseInt($(".fig-edit1").val());
+// var b = parseInt($(".fig-edit2").val());
 //
-//   else {
-//       $( this ).val( input + '.00');
-//   }
+// $(".fig-edit1").blur(function() {
+//   // $('#total').text('');
+//   $('.1').append(a);
+// });
+//
+// $(".fig-edit2").blur(function() {
+//   $('#total').text('');
+//   $('#total').append(a+b);
 // });
 
-$(".inner").blur(function() {
-  var input = $(this).val();
-  if($(this).val() == ""){
-    return false;
-  }
-  if($(this).val().indexOf('£' ) > -1){
-    return false;
-  }
-  if($(this).val() != "£"){
-    $( this ).val( '£' + input );
-  }
-});
 
 
-$(".inner").blur(function() {
-  var input = $(this).val();
-  if($(this).val() == ""){
-     return false;
-   }
-  if($(this).val().indexOf('.' ) > -1){
-     return false;
-   }
-  if($(this).val() != "."){
-     $( this ).val( input + '.00' );
-   }
+// $(".fig-edit1").blur(function() {
+//   var fig1 = $(this).val();
+//   var fig2 = $(".fig-edit2").val();
+//   $('.1').text('');
+//   $('.1').append(fig1);
+//   var total = parseInt(fig1) + parseInt(fig2);
+//       $("#total").text('');
+//       $("#total").append(total);
+//
+// });
+//
+// $(".fig-edit2").blur(function() {
+//   var fig2 = $(this).val();
+//   var fig1 = $(".fig-edit1").val();
+//   $('.2').text('');
+//   $('.2').append(fig2);
+//   var total = parseInt(fig1) + parseInt(fig2);
+//       $("#total").text('');
+//       $("#total").append(total);
+//
+// });
+
+$(function() {
+  $('.fig-edit1').on("input", function() {
+    var fig1 = $(this).val();
+    var fig2 = $(".fig-edit2").val();
+    $('.1').text('');
+    $('.1').append(fig1);
+    var total = parseInt(fig1) + parseInt(fig2);
+        $("#total").text('');
+        $("#total").append(total);
+    if (fig1 == '') {
+      $('.1').append('7000');
+      var one = $('.1').text();
+      var total = parseInt(one) + parseInt(fig2);
+      $("#total").text('');
+      $("#total").append(total);
+    }
+
+    if (fig2 == '' && fig1 == '') {
+      $('#total').append('13000');
+    }
+
+  });
 });
+
+$(function() {
+  $('.fig-edit1').on("input", function() {
+    var fig1 = $(this).val();
+    var two = $('.2').text();
+    var total = parseInt(two) + parseInt(fig1);
+    $("#total").text('');
+    $("#total").append(total);
+
+    var fig2 = $(".fig-edit2").val();
+    if (fig2 == '' && fig1 == '') {
+      $('#total').append('13000');
+    }
+  });
+});
+
+$(function() {
+  $('.fig-edit2').on("input", function() {
+    var fig2 = $(this).val();
+      var fig1 = $(".fig-edit1").val();
+      $('.2').text('');
+      $('.2').append(fig2);
+      var total = parseInt(fig1) + parseInt(fig2);
+          $("#total").text('');
+          $("#total").append(total);
+      if (fig2 == '') {
+        $('.2').append('6000');
+        var two = $('.2').text();
+        var total = parseInt(two) + parseInt(fig1);
+        $("#total").text('');
+        $("#total").append(total);
+      }
+
+      if (fig2 == '' && fig1 == '') {
+        $('#total').append('13000');
+      }
+
+  });
+});
+
+$(function() {
+  $('.fig-edit2').on("input", function() {
+    var fig2 = $(this).val();
+    var one = $('.1').text();
+    var total = parseInt(one) + parseInt(fig2);
+    $("#total").text('');
+    $("#total").append(total);
+
+    var fig1 = $(".fig-edit1").val();
+    if (fig2 == '' && fig1 == '') {
+      $('#total').append('13000');
+    }
+  });
+});
+
+// // // // // // //
+// var one = $('.1').text();
+// var two = $(".fig-edit2").val();
+// var total2 = parseInt(one) + parseInt(two);
+//     $("#total").text('');
+//     $("#total").append(total2);
+// // // // // // //
+
+
+// $(".fig-edit2").blur(function() {
+//   var fig2 = $(this).val();
+//   $('.2').text('');
+//   $('.2').append(fig2);
+//   $('#total').append(fig2);
+// });
+
+// var total = 0;
+// $('.fig').each(function(){
+//   total += parseInt($(this).text());
+// });
+// $('#total').append("£"+total);
+
+
 
 
 $('#confirmSubmit').click(function() {
